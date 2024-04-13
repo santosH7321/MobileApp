3a25c7b80e44a8ea8bb490ec1213824ad46b56f9
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-04-13 10:20:17 +0530

    setUp

---
+++ app/profile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text className="flex justify-center items-center text-2xl text-pink-500 font-bold p-5">Profile Page</Text>
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
