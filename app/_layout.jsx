656bc3181824538a1b75f244f2cdd1b6eda8f869
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-04-14 22:32:08 +0530

    backend

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { SplashScreen, Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
 
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -18,24 +21,32 @@ const RootLayout = () => {
     "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   });
 
-
   useEffect(() => {
-    if(error) throw error;
+    if (error) throw error;
 
-    if(fontsLoaded) SplashScreen.hideAsync();
-  },[fontsLoaded, error])
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
 
-  if(!fontsLoaded && !error) return null;
+  if (!fontsLoaded) {
+    return null;
+  }
 
+  if (!fontsLoaded && !error) {
+    return null;
+  }
 
-  return(
-    <Stack>
-        <Stack.Screen name='index' options={{headerShown: false}} />
-        <Stack.Screen name='(auth)' options={{headerShown: false}} />
-        <Stack.Screen name='(tabs)' options={{headerShown: false}} />
-        {/* <Stack.Screen name='/search/[query]' options={{headerShown: false}} /> */}
-    </Stack>
-  )
-}
+  return (
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,5 +1,5 @@
 import { TouchableOpacity, Text } from 'react-native'
-import React from 'react'
+
 
 const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}) => {
   return (
